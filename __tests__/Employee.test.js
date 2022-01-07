689e90610198410ffbf82eecd2d66e24a08847b1
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-01-07 18:32:12 -0500

    Employee tests and object constructor

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require("../lib/Employee");
+
+test("creates an employee object", () => {
+    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+
+    expect(employee.name).toBe("Sauron");
+    expect(employee.id).toBe("000");
+    expect(employee.email).toBe("sauron@mordor.com");
+    expect(employee.role).toBe("Employee");
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+
+    expect(employee.getName()).toBe(employee.name);
+});
+
+test("gets employee's ID", () => {
+    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id));
+});
+
+test("gets employee's email address", () => {
+    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+
+    expect(employee.getRole()).toBe(employee.role);
+});
