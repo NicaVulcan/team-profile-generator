46c64574e3cea36affd2a68c8de34633e8f93877
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-01-08 00:39:55 -0500

    Manager tests and object constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,34 +1,33 @@
 const Employee = require("../lib/Employee");
 
 test("creates an employee object", () => {
-    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+    const employee = new Employee("Sauron", "000", "sauron@mordor.com");
 
     expect(employee.name).toBe("Sauron");
     expect(employee.id).toBe("000");
     expect(employee.email).toBe("sauron@mordor.com");
-    expect(employee.role).toBe("Employee");
 });
 
 test("gets employee's name", () => {
-    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+    const employee = new Employee("Sauron", "000", "sauron@mordor.com");
 
     expect(employee.getName()).toBe(employee.name);
 });
 
 test("gets employee's ID", () => {
-    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+    const employee = new Employee("Sauron", "000", "sauron@mordor.com");
 
     expect(employee.getId()).toEqual(expect.stringContaining(employee.id));
 });
 
 test("gets employee's email address", () => {
-    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+    const employee = new Employee("Sauron", "000", "sauron@mordor.com");
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
 });
 
 test("gets employee's role", () => {
-    const employee = new Employee("Employee", "Sauron", "000", "sauron@mordor.com");
+    const employee = new Employee("Sauron", "000", "sauron@mordor.com");
 
-    expect(employee.getRole()).toBe(employee.role);
+    expect(employee.getRole()).toBe("Employee");
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,16 @@
+const Manager = require("../lib/Manager");
 
+test("creates a manager object", () => {
+    const manager = new Manager("Saruman", "001", "saruman@mordor.com", "100");
+
+    expect(manager.name).toBe("Saruman");
+    expect(manager.id).toBe("001");
+    expect(manager.email).toBe("saruman@mordor.com");
+    expect(manager.office).toBe("100");
+});
+
+test("gets employee's role", () => {
+    const employee = new Manager("Saruman", "001", "saruman@mordor.com", "100");
+
+    expect(employee.getRole()).toBe("Manager");
+});
